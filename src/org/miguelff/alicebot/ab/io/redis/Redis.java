9a4b8c4d7a94025bd1f41f292decbf36f94c9d34
Miguel <miguelfernandezfernandez@gmail.com>
2013-02-03 21:04:36 +0100

    added sanity checks

--- src/org/miguelff/alicebot/ab/io/redis/Redis.java
+++ src/org/miguelff/alicebot/ab/io/redis/Redis.java
@@ -3,6 +3,7 @@ package org.miguelff.alicebot.ab.io.redis;
 import java.net.URI;
 
 import org.miguelff.alicebot.ab.Config;
+import org.miguelff.alicebot.ab.ResourceProvider;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -15,13 +16,14 @@ public class Redis {
 	static final String PATH_SEPARATOR = "/";
 	private static JedisPool pool;
 			
-	static {
+	static { 
 		URI redisURI = Config.REDISTOGO_URL;
 	    pool = new JedisPool(new JedisPoolConfig(),
 	            redisURI.getHost(),
 	            redisURI.getPort(),
 	            Protocol.DEFAULT_TIMEOUT,
 	            redisURI.getUserInfo() != null ? redisURI.getUserInfo().split(":",2)[1] : null);
+	    check();
 	}
 			
 	
@@ -35,6 +37,26 @@ public class Redis {
 	static String withPrefix(String key) {
 		//at least one char more
 		return key + "?*";
+	}
+	
+	public static void check() {
+		try {
+			exec(new RedisCommand<Void>() {
+				@Override
+				public Void execute(Jedis redis) {
+					redis.keys("*");
+					return null;
+				}
+			});
+			ResourceProvider.Log.info("Sanity check PASSED for Redis");
+		}catch(Exception e){
+			ResourceProvider.Log.error("Sanity check FAILED for Redis:");
+			ResourceProvider.Log.error("===========================");
+			ResourceProvider.Log.error(String.format("REDISTOGO_URL: %s", Config.REDISTOGO_URL));
+			ResourceProvider.Log.error(e.getMessage());
+			ResourceProvider.Log.error("===========================");
+			System.exit(-1);
+		}
 	}
 	
 }
--- src/org/miguelff/alicebot/ab/io/s3/S3.java
+++ src/org/miguelff/alicebot/ab/io/s3/S3.java
@@ -1,6 +1,7 @@
 package org.miguelff.alicebot.ab.io.s3;
 
 import org.miguelff.alicebot.ab.Config;
+import org.miguelff.alicebot.ab.ResourceProvider;
 
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -11,7 +12,7 @@ import com.amazonaws.services.s3.AmazonS3Client;
  * @author miguel
  *
  */
-class S3 {
+class S3 { 
 
 	static final String PATH_SEPARATOR = "/";
 	private static final String BUCKET_NAME;
@@ -22,6 +23,7 @@ class S3 {
 		String secretKey = Config.AWS_SECRET;		
 		BUCKET_NAME = Config.AWS_S3_BUCKET_NAME;
 		s3 = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey));
+		check();
 	}
 	
 	static AmazonS3Client client(){
@@ -30,5 +32,23 @@ class S3 {
 	
 	static String bucket(){
 		return BUCKET_NAME;
+	}
+
+	private static void check() {
+		try {
+			boolean ok = client().listObjects(bucket()).getObjectSummaries().size() > 0;
+			if (!ok){
+				throw new IllegalStateException("Objects retrieved: 0");
+			}else{
+				ResourceProvider.Log.info("Sanity check PASSED for S3");
+			}
+		}catch(Exception e){
+			ResourceProvider.Log.error("Sanity check FAILED for S3:");
+			ResourceProvider.Log.error("===========================");
+			ResourceProvider.Log.error(String.format("AWS_ACCESS_KEY: %s, ",Config.AWS_ACCESS_KEY, Config.AWS_SECRET, Config.AWS_S3_BUCKET_NAME));
+			ResourceProvider.Log.error(e.getMessage());
+			ResourceProvider.Log.error("===========================");
+			System.exit(-1);
+		}
 	}
 }
